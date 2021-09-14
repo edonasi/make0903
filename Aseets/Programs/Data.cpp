b7e04b9c3252c96566c596908066045bc4113142
edonasi <hj-g20030@sist.ac.jp>
2021-09-14 11:48:21 +0900

    セーブデータ読み込み関数作成

---
+++ Aseets/Programs/Data.cpp
@@ -0,0 +1,72 @@
+#include "Data.h"
+
+/*
+http://www.isl.ne.jp/pcsp/beginC/C_Language_16.html
+https://9cguide.appspot.com/17-01.html
+*/
+
+/// <summary>
+/// SCVファイルを読み込む
+/// </summary>
+/// <param name="path">CSVファイルのパス</param>
+/// <param name="chara">キャラデータ構造体の先頭アドレス</param>
+/// <param name="dataMax">CSVファイルの最大値</param>
+/// <param name="isHeader">CSVファイルの1行目がヘッダならTRUE</param>
+/// <returns>TRUEで読み込めた</returns>
+BOOL LoadScvChara(const char* path, CHARA_DATA_SAMPLE* chara, int dataMax, BOOL isHeader) 
+{
+	FILE* fp;					//ファイルポインタ
+	char getChar = '\0';		//取得する文字
+	int loopCnt = 0;			//ループカウンタ
+	errno_t fileErr = NULL;		//ファイル読み込みエラーを確認
+	int isEOF = NULL;			//ファイルの終わりかチェック
+
+	fileErr = fopen_s(&fp, path, "r");	//ファイルを読み込みモード(r)で開く
+	if (fileErr != NULL) 
+	{
+		MessageBox(GetMainWindowHandle(), path, CSV_LOAD_ERR_TITLE, MB_OK);	//読み込みエラー
+		return FALSE;
+	}
+
+	//	↓ここから正常に読み込めた時の処理
+
+	//csvの1行目がヘッダのとき
+	if (isHeader == TRUE) 
+	{
+		//1文字ずつ取り出し、改行をヒントに1行目を読み飛ばす
+		while (getChar != '\n') { getChar = fgetc(fp); }
+	}
+
+	//実際にデータを格納する
+	while (isEOF != EOF)	//ファイルの最後ではないとき繰り返す
+	{
+		//csvのデータの1行分を読み取る
+		//fp = カーソルのイメージ
+		isEOF = fscanf_s(
+			fp,
+			CSV_CHARA_FORMAT,
+			&(chara + loopCnt)->no,
+			&(chara + loopCnt)->name[0], STR_MAX,
+			&(chara + loopCnt)->hp,
+			&(chara + loopCnt)->atk,
+			&(chara + loopCnt)->def,
+			&(chara + loopCnt)->hpMax,
+			&(chara + loopCnt)->atkMax,
+			&(chara + loopCnt)->defMax
+			);
+
+		//次のデータへ
+		loopCnt++;
+	}
+
+	//データの数が違うとき
+	if (loopCnt - 1 != dataMax) 
+	{
+		MessageBox(GetMainWindowHandle(), path, CSV_DATA_ERR_TITLE, MB_OK);
+		return FALSE;
+	}
+
+	fclose(fp);	//ファイルを閉じる
+
+	return TRUE;
+}
---
+++ Aseets/Programs/Data.h
@@ -0,0 +1,42 @@
+#pragma once
+
+//ゲームのメインヘッダファイル
+#include "game.h"
+
+//	マクロ定義
+//エラーメッセージ
+#define CSV_LOAD_ERR_TITLE TEXT("CSV読み込みエラー")
+
+//キャラクターデータのパス
+#define CSV_PATH_ENEMY TEXT("./Aseets/Data/enemy.csv")
+
+//データの書式指定子
+//キャラクタの書式指定子(%[^,]は文字列(,が来るまで))
+#define CSV_CHARA_FORMAT TEXT("%d,%[^,],%d,%d,%d,%d,%d,%d")
+
+//読み込み数エラーメッセージ
+#define CSV_DATA_ERR_TITLE TEXT("CSV数エラー")
+
+//敵データ
+#define ENEMY_MAX 5
+
+//キャラデータ構造体(サンプル)
+struct CHARA_DATA_SAMPLE
+{
+	int no;					//番号
+	char name[STR_MAX];		//名前
+
+	//実際に使う値
+	int hp;					//HP
+	int atk;				//攻撃力
+	int def;				//防御力
+
+	//最大値
+	int hpMax;				//最大HP
+	int atkMax;				//最大攻撃力
+	int defMax;				//最大防御力
+};
+
+extern CHARA_DATA_SAMPLE enemy[ENEMY_MAX];
+
+extern BOOL LoadScvChara(const char* path, CHARA_DATA_SAMPLE* chara, int dataMax, BOOL isHeader);
